23d36d4c77730233fa059d06c7daa023a0c43ea7
Olivia <olivia.cowan@hotmail.com>
2022-01-25 23:49:01 +0000

    added the edit route

--- express_server.js
+++ express_server.js
@@ -11,10 +11,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get('/', (reg, res) => {
-//   res.send('Hello!');
-// });
-
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase
@@ -29,14 +25,6 @@ app.get('/urls/new', (reg, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.post('/urls', (req, res) => {
@@ -59,6 +47,13 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   console.log(urlDatabase);
   res.redirect('/urls');
 });
+
+app.post('/urls/:id', (req, res) => {
+  const shortURL = req.params.id;
+  const newLongURL = req.body.longURL;
+  urlDatabase[shortURL] = newLongURL;
+  res.redirect('/urls');
+})
 
 function generateRandomString() {
   const randString = Math.random().toString(24);
