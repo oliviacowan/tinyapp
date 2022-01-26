04048fbd923276f634372ad293fc417960847dca
Olivia <olivia.cowan@hotmail.com>
2022-01-26 03:58:48 +0000

    added login/logout in views and server. (thought i made a branch but it isn't working so committing this way

--- express_server.js
+++ express_server.js
@@ -1,59 +1,74 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
+
 const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//main page
 app.get('/urls', (req, res) => {
   const templateVars = {
+    username: req.cookies["username"],
     urls: urlDatabase
   };
   res.render('urls_index', templateVars);
 })
-
-app.get('/urls/new', (reg, res) => {
-  res.render('urls_new');
+//create page
+app.get('/urls/new', (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+  res.render('urls_new', templateVars);
 });
-
+//edit page
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
-
+//creates a new tinyURL
 app.post('/urls', (req, res) => {
-  //console.log(req.body);
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   return res.redirect(`/urls/${shortURL}`);
 });
-
+//redirects to longURL from the shortURL link
 app.get('/u/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
-  console.log("heyyyyy",longURL);
   res.redirect(longURL);
 });
-
+//deletes a url
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
-  console.log(urlDatabase);
   res.redirect('/urls');
 });
-
+//edits a url
 app.post('/urls/:id', (req, res) => {
   const shortURL = req.params.id;
   const newLongURL = req.body.longURL;
   urlDatabase[shortURL] = newLongURL;
   res.redirect('/urls');
-})
+});
+//creates cookie/logs in
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+//logs out
+app.post('/logout', (req, res) => {
+  res.clearCookie('username', req.body.username);
+  res.redirect('/urls');
+});
 
 function generateRandomString() {
   const randString = Math.random().toString(24);
